95f2cb41a05d8770d1914317666a25a33a84809e
Javier pinzon <jspinzonr@misena.edu.co>
2021-03-24 10:54:43 -0500

    First commit

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+      $request->validate([
+        'name' => 'required|string',
+        'lastname' => 'required|string',
+        'age' => 'required|numeric',
+        'profession' => 'required|string'
+      ]);
+      $result = [
+        'name' => $request->name,
+        'lastname' => $request->lastname,
+        'age' => $request->age,
+        'profession' => $request->profession,
+      ];
+      var_dump($result);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/js/components/RegisterForm.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-8">
+        <div class="card">
+          <div class="card-header">Registro</div>
+
+          <div class="card-body">
+            <form @submit.prevent="sendData">
+              <div class="form-group">
+                <label for="name">Nombres:</label>
+                <input
+                  type="text"
+                  v-model="form.name"
+                  class="form-control"
+                  id="name"
+                />
+              </div>
+              <div class="form-group">
+                <label for="lastname">Apellidos:</label>
+                <input
+                  type="text"
+                  v-model="form.lastname"
+                  class="form-control"
+                  id="lastname"
+                />
+              </div>
+              <div class="form-group">
+                <label for="age">Edad:</label>
+                <input
+                  type="number"
+                  v-model="form.age"
+                  class="form-control"
+                  id="age"
+                />
+              </div>
+              <div class="form-group">
+                <label for="profession">Profesión:</label>
+                <input
+                  type="text"
+                  v-model="form.profession"
+                  class="form-control"
+                  id="profession"
+                />
+              </div>
+              <button type="submit" class="btn btn-primary">Enviar</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        name: null,
+        lastname: null,
+        age: 0,
+        profession: null
+      }
+    };
+  },
+
+  methods: {
+    sendData() {
+      axios.post('register',this.form)
+      .then(res => {
+        console.log(res)
+      })
+      .catch(err => {
+        console.error(err);
+      })
+    }
+  },
+};
+</script>
